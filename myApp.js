f58c6cbc7bca5699ef414b9896f26ab900ec1eed
Fihliwe Mncube <siyabulelmncube@gmail.com>
2024-11-25 16:24:06 +0000

    serve the public files using express

--- myApp.js
+++ myApp.js
@@ -1,14 +1,13 @@
 let express = require('express');
 let app = express();
 
-absolutePath = __dirname + "/views/index.html";
-
+index_html = __dirname + "/views/index.html";
 app.get("/", (req, res) =>{
-    res.sendFile(absolutePath);
+    res.sendFile(index_html);
 });
 
-
-
+style_css = __dirname + "/public";
+app.use("/public", express.static(style_css))
 
 
 
